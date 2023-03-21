8276fadbda22ad0f71ae7631a85bb19dd70a489f
Pavel <paxa-tron@mail.ru>
2023-03-21 22:42:27 +0300

    Add Pagination in Index.

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -6,10 +6,14 @@ namespace SportsStore.Controllers;
 public class HomeController:Controller
 {
     private IStoreRepository _repository;
+    public int PageSize = 4;
 
     public HomeController(IStoreRepository repository)
     {
         _repository = repository;
     }
-    public IActionResult Index() => View(_repository.Products);
+    public ViewResult Index(int productPage = 1) => View(_repository.Products
+        .OrderBy(p => p.ProductID)
+        .Skip((productPage - 1) * PageSize)
+        .Take(PageSize));
 }
