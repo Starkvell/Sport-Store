422bf97428965dd7694f09820622e6e401385762
Pavel <paxa-tron@mail.ru>
2023-03-25 23:36:10 +0300

    Add the View Model Data.

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -1,19 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
+using SportsStore.Models.ViewModels;
 
-namespace SportsStore.Controllers;
-
-public class HomeController:Controller
+namespace SportsStore.Controllers
 {
-    private IStoreRepository _repository;
-    public int PageSize = 4;
-
-    public HomeController(IStoreRepository repository)
+    public class HomeController : Controller
     {
-        _repository = repository;
+        private IStoreRepository _repository;
+        public int PageSize = 4;
+
+        public HomeController(IStoreRepository repo)
+        {
+            _repository = repo;
+        }
+
+        public ViewResult Index(int productPage = 1)
+            => View(new ProductsListViewModel
+            {
+                Products = _repository.Products
+                    .OrderBy(p => p.ProductID)
+                    .Skip((productPage - 1) * PageSize)
+                    .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = productPage,
+                    ItemsPerPage = PageSize,
+                    TotalItems = _repository.Products.Count()
+                }
+            });
     }
-    public ViewResult Index(int productPage = 1) => View(_repository.Products
-        .OrderBy(p => p.ProductID)
-        .Skip((productPage - 1) * PageSize)
-        .Take(PageSize));
 }
