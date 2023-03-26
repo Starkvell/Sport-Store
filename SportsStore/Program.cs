076136fcb07718ec9c9075e61bae16dd74d45b40
Pavel <paxa-tron@mail.ru>
2023-03-26 18:12:16 +0300

    Add razor pages from cart.

--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -12,6 +12,8 @@ builder.Services.AddDbContext<StoreDbContext>(opts =>
 
 builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
 
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
 
 app.UseStaticFiles();
@@ -31,6 +33,7 @@ app.MapControllerRoute("pagination",
     new { Controller = "Home", action = "Index", productPage = 1 });
 
 app.MapDefaultControllerRoute();
+app.MapRazorPages();
 
 SeedData.EnsurePopulated(app);
 
