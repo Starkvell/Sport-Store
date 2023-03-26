861c0bbc902222bf639c70a39c3d490b7d59e1ea
Pavel <paxa-tron@mail.ru>
2023-03-26 17:21:53 +0300

    Add Category-specific product counts and unit test from that.

--- SportsStore.Tests/HomeControllerTests.cs
+++ SportsStore.Tests/HomeControllerTests.cs
@@ -54,7 +54,7 @@ public class HomeControllerTests
 
         // Act
         ProductsListViewModel result =
-            controller.Index(null,2)?.ViewData.Model as ProductsListViewModel ?? new ProductsListViewModel();
+            controller.Index(null, 2)?.ViewData.Model as ProductsListViewModel ?? new ProductsListViewModel();
 
         // Assert
         Product[] prodArray = result.Products.ToArray();
@@ -81,7 +81,7 @@ public class HomeControllerTests
             new HomeController(mock.Object) { PageSize = 3 };
         // Act
         ProductsListViewModel result =
-            controller.Index(null,2)?.ViewData.Model as ProductsListViewModel
+            controller.Index(null, 2)?.ViewData.Model as ProductsListViewModel
             ?? new ProductsListViewModel();
         // Assert
         PagingInfo pageInfo = result.PagingInfo;
@@ -90,18 +90,20 @@ public class HomeControllerTests
         Assert.Equal(5, pageInfo.TotalItems);
         Assert.Equal(2, pageInfo.TotalPages);
     }
-    
+
     [Fact]
-    public void Can_Filter_Products() {
+    public void Can_Filter_Products()
+    {
         // Arrange
         // - create the mock repository
         Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
-        mock.Setup(m => m.Products).Returns((new Product[] {
-            new Product {ProductID = 1, Name = "P1", Category = "Cat1"},
-            new Product {ProductID = 2, Name = "P2", Category = "Cat2"},
-            new Product {ProductID = 3, Name = "P3", Category = "Cat1"},
-            new Product {ProductID = 4, Name = "P4", Category = "Cat2"},
-            new Product {ProductID = 5, Name = "P5", Category = "Cat3"}
+        mock.Setup(m => m.Products).Returns((new Product[]
+        {
+            new Product { ProductID = 1, Name = "P1", Category = "Cat1" },
+            new Product { ProductID = 2, Name = "P2", Category = "Cat2" },
+            new Product { ProductID = 3, Name = "P3", Category = "Cat1" },
+            new Product { ProductID = 4, Name = "P4", Category = "Cat2" },
+            new Product { ProductID = 5, Name = "P5", Category = "Cat3" }
         }).AsQueryable<Product>());
         // Arrange - create a controller and make the page size 3 items
         HomeController controller = new HomeController(mock.Object);
@@ -113,5 +115,34 @@ public class HomeControllerTests
         Assert.Equal(2, result.Length);
         Assert.True(result[0].Name == "P2" && result[0].Category == "Cat2");
         Assert.True(result[1].Name == "P4" && result[1].Category == "Cat2");
+    }
+
+    [Fact]
+    public void Generate_Category_Specific_Product_Count()
+    {
+        // Arrange
+        Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+        mock.Setup(m => m.Products).Returns(new Product[]
+        {
+            new Product { ProductID = 1, Name = "P1", Category = "Cat1" },
+            new Product { ProductID = 2, Name = "P2", Category = "Cat2" },
+            new Product { ProductID = 3, Name = "P3", Category = "Cat1" },
+            new Product { ProductID = 4, Name = "P4", Category = "Cat2" },
+            new Product { ProductID = 5, Name = "P5", Category = "Cat3" }
+        }.AsQueryable());
+        HomeController target = new HomeController(mock.Object);
+        target.PageSize = 3;
+        Func<ViewResult, ProductsListViewModel?> GetModel = result
+            => result?.ViewData?.Model as ProductsListViewModel;
+        // Action
+        int? res1 = GetModel(target.Index("Cat1"))?.PagingInfo.TotalItems;
+        int? res2 = GetModel(target.Index("Cat2"))?.PagingInfo.TotalItems;
+        int? res3 = GetModel(target.Index("Cat3"))?.PagingInfo.TotalItems;
+        int? resAll = GetModel(target.Index(null))?.PagingInfo.TotalItems;
+        // Assert
+        Assert.Equal(2, res1);
+        Assert.Equal(2, res2);
+        Assert.Equal(1, res3);
+        Assert.Equal(5, resAll);
     }
 }
